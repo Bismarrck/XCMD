e9e39debbb6b237490e51f8f888becf0ad258ac8
Bismarrck <Bismarrck@me.com>
2014-05-20 18:28:26 +0800

    Initial Commit

---
+++ XCMD/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  XCMD
+//
+//  Created by Bismarrck on 5/20/14.
+//  Copyright (c) 2014 TCCL. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
